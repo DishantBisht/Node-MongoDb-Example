21bd7e37c265ad7c9febc774106e42eb177c2680
Dishant Bisht <dishantbisht32@gmail.com>
2020-05-22 14:56:14 +0530

    Resolved callback hell

--- index.js
+++ index.js
@@ -12,22 +12,24 @@ MongoClient.connect(url, (err, client) => {
     const db = client.db(dbname);
 
     console.log('Connected correctly to server');
-        dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
-        "dishes", (result) => {
+        dboper.insertDocument(db, { name: "Vadonut", description: "Test"}, "dishes")
+        .then((result) => {
             console.log("Insert Document:\n", result.ops);
 
-            dboper.findDocuments(db, "dishes", (docs) => {
+            dboper.findDocuments(db, "dishes")
+            .then((docs) => {
                 console.log("Found Documents:\n", docs);
 
-                dboper.updateDocument(db, { name: "Vadonut" },
-                    { description: "Updated Test" }, "dishes",
-                    (result) => {
+                dboper.updateDocument(db, { name: "Vadonut" }, { description: "Updated Test" }, "dishes")
+                    .then((result) => {
                         console.log("Updated Document:\n", result.result);
 
-                        dboper.findDocuments(db, "dishes", (docs) => {
+                        dboper.findDocuments(db, "dishes")
+                        .then((docs) => {
                             console.log("Found Updated Documents:\n", docs);
                             
-                            db.dropCollection("dishes", (result) => {
+                            db.dropCollection("dishes")
+                            .then((result) => {
                                 console.log("Dropped Collection: ", result);
 
                                 client.close();
